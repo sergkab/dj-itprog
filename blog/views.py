9e3f70623dd687e8dc069d6fd2ce64f618f9cbc3
sergkab <sergkab@gmail.com>
2022-11-25 13:15:08 -0800

    I DID IT!!! plug static. blog/css/style.css  * 2022 11 25

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.conf import settings
 
 # Create your views here.
 def home(request):
@@ -8,7 +9,13 @@ def home(request):
     return render( request, 'blog/home.html', { 'posts': posts } )
 
 def test(request):
-    return HttpResponse('<h2> Hello! I am Test 2022 11 06 </h2> <p> <h2> Test Page 05 after Amend  </h2> </p> ')
+    #return HttpResponse('<h2> Hello! I am Test 2022 11 06 </h2> <p> <h2> Test Page 05 after Amend  </h2> </p> ')
+    statdirs = settings.STATICFILES_DIRS
+    staturl = settings.STATIC_URL
+    basdir = settings.BASE_DIR
+    statroot = settings.STATIC_ROOT
+    context = { 'statdirs': statdirs, 'staturl': staturl, 'basdir': basdir, 'statroot': statroot }
+    return render( request, 'blog/test.html', context )
 
 def first(request):
     #return HttpResponse('<h2> Hello! I am Test/first 2022 11 22 </h2> <p> <h2> Test Page 06   </h2> </p> ')
